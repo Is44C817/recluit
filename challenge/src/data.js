e44f1143007ca8d0ef7da44d7c5d3836e2a34b87
Is44C817 <isaac.sanchez.sandoval@gmail.com>
2022-12-20 10:48:09 -0600

    create component headar and produclist

---
+++ challenge/src/data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Manager A',
+        price: 300,
+        quantity: 1
+    },
+    {
+        id: 2,
+        name: 'Manager B',
+        price: 300,
+        quantity: 1
+    },
+    {
+        id: 3,
+        name: 'Developer',
+        price: 1000,
+        quantity: 1
+    },
+    {
+        id: 4,
+        name: 'QA Tester',
+        price: 500,
+        quantity: 1
+    }
+]
